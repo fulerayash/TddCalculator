95366e6387abae6e720ec5c885aee519f691497e
fulerayash <fulerayash@gmail.com>
2019-09-13 10:30:21 +0530

    First Test for positive number as a result on two positive number as parameter

--- TddCalculator.Test/CalculatorTest.cs
+++ TddCalculator.Test/CalculatorTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddCalculator.Library;
 
 namespace TddCalculator.Test
 {
@@ -7,8 +8,13 @@ namespace TddCalculator.Test
     public class CalculatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnPositveNumberOnPassingTwoPositiveNumber()
         {
+            Calculator CalObj = new Calculator();
+            int num1 = 5;
+            int num2 = 7;
+            int resultSum = CalObj.Addition(num1, num2);
+            Assert.AreEqual(12, resultSum, "Testing result for addition of 5 and 7");
         }
     }
 }
